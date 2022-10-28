6299252ea14c58db4b96407d5e2373a1db4c54d3
Cristian Sanchez Flores <andygign40@gmail.com>
2022-10-27 21:04:13 -0500

    Network with cache fallback - Para las peticiones a nuestra API

--- public/sw.js
+++ public/sw.js
@@ -75,8 +75,7 @@ self.addEventListener('activate', e => {
 
 self.addEventListener( 'fetch', e => {
 
-
-    const respuesta = caches.match( e.request ).then( res => {
+    /** const respuesta = caches.match( e.request ).then( res => {
 
         if ( res ) {
             
@@ -92,9 +91,25 @@ self.addEventListener( 'fetch', e => {
 
         }
 
-    });
+    }); */
 
+    let respuesta;
 
+    if (e.request.url.includes("/api")) {
+      //return respuesta?
+      respuesta = manejoApiMensajes(DYNAMIC_CACHE, e.request);
+    } else {
+      respuesta = caches.match(e.request).then((res) => {
+        if (res) {
+          actualizaCacheStatico(STATIC_CACHE, e.request, APP_SHELL_INMUTABLE);
+          return res;
+        } else {
+          return fetch(e.request).then((newRes) => {
+            return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+          });
+        }
+      });
+    }
 
     e.respondWith( respuesta );
 
